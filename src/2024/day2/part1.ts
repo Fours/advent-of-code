fbd40c9336477c598568e7552af4a39a573a6ecd
Kyle Willett <kylewillett@yahoo.com>
2026-08-26 10:35:09 -0500

    Added day 2 part 1

---
+++ src/2024/day2/part1.ts
@@ -0,0 +1,31 @@
+import fs from "node:fs"
+
+function part1(input: string): number {
+
+    const reports: number[][] = input.split("\n").map(row => {
+        return row.split(" ").map(Number)
+    })
+
+    let safeCount = 0
+
+    reports.forEach(report => {
+        let isSafe = true
+        const isAsc = report[0] < report[1]
+
+        for (let i = 0; i < report.length - 1; i++) {
+            const diff = isAsc ? report[i + 1] - report[i] : report[i] - report[i + 1]
+            if (diff < 1 || diff > 3) {
+                isSafe = false
+            }
+        }
+
+        if (isSafe) safeCount += 1
+    })
+
+    return safeCount
+}
+
+const input = fs.readFileSync("src/2024/day2/input.txt", "utf8")
+const solution = part1(input)
+
+console.log("solution:", solution)
