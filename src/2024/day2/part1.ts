e71aa273e369f08262eee2d9fcbb64bbc946000e
Kyle Willett <kylewillett@yahoo.com>
2026-08-26 10:44:28 -0500

    Added performance log and optimized loop

--- src/2024/day2/part1.ts
+++ src/2024/day2/part1.ts
@@ -12,7 +12,7 @@ function part1(input: string): number {
         let isSafe = true
         const isAsc = report[0] < report[1]
 
-        for (let i = 0; i < report.length - 1; i++) {
+        for (let i = 0; isSafe && i < report.length - 1; i++) {
             const diff = isAsc ? report[i + 1] - report[i] : report[i] - report[i + 1]
             if (diff < 1 || diff > 3) {
                 isSafe = false
@@ -26,6 +26,9 @@ function part1(input: string): number {
 }
 
 const input = fs.readFileSync("src/2024/day2/input.txt", "utf8")
+const startTime = performance.now()
 const solution = part1(input)
+const endTime = performance.now()
+console.log(`That solution took ${(endTime - startTime).toFixed(4)} milliseconds.`);
 
 console.log("solution:", solution)
